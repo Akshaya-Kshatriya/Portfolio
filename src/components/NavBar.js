00fadea36bc0462d7bbfcffe22f1b8b9d3bd4c54
Akshaya-Kshatriya <akshayavenkatesh2821@gmail.com>
2024-12-25 23:25:01 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -52,8 +52,8 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/akshaya-v-b22306343"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/akshaya__kshatriya/"><img src={navIcon3} alt="" /></a>
+                <a href="https://github.com/Akshaya-Kshatriya/"><img src={navIcon2} alt="" /></a>
+                
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
